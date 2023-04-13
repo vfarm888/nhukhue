9dfb501b49b206af54f78674b3d4339cc4c00cc0
vfarm888 <130117126+vfarm888@users.noreply.github.com>
2023-04-13 20:37:31 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+
+while :; do
+    
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm0v9kcghga7njq73d4re -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
+    
+sleep 5;
+
+done
